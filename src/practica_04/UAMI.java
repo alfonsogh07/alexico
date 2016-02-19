665411c64f4546a01eb6a06e6ce252360e22474e
Sandy <Sandy@Toshiba>
2016-02-19 14:43:49 -0600

    Primer version

--- src/practica_04/UAMI.java
+++ src/practica_04/UAMI.java
@@ -1,5 +1,10 @@
 package practica_04;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +16,10 @@ package practica_04;
  * @author poncho
  */
 class UAMI {
-    
+      public static File archivo_tpl, archivo_err;
+    public static PrintWriter wr1,wr2;
+    public static BufferedWriter bw1,bw2;
+    public static FileReader Archivo_fte;
+    public static int linea = 1;
+    public static String tokenval = null;
 }
